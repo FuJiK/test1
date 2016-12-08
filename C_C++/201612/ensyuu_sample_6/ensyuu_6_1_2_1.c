e29a43656882f33579928da507cfee9b06d9ad16
fujiokk <k.fujioka0727@gmail.com>
2016-12-08 19:15:47 +0900

    20161208 //from workstation

---
+++ C_C++/201612/ensyuu_sample_6/ensyuu_6_1_2_1.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+int main(int argc, char const *argv[]) {
+  char moji;
+  printf("文字を入力しなさい　:" );
+  scanf("%c",&moji );
+
+  if ('A' <= moji && moji <= 'Z') {
+    printf("英大文字です\n");
+  }else if ('a' <= moji && moji <= 'z') {
+    printf("英小文字です\n");
+  }else if ('0' <= moji && moji <= '9') {
+    printf("数字です\n");
+  }else{
+    printf("英字でも数字でもありません。\n");
+  }
+
+  //printf("moji:%c\n",moji );
+
+  return 0;
+}
---
+++ C_C++/201612/ensyuu_sample_6/ensyuu_6_1_3_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  char kamoku;
+  int ten;
+
+  printf("科目を選択して下さい(k:国語、s:数学)　：\n" );
+  scanf("%*c%c", &kamoku );
+
+  if (kamoku == k) {
+      if (/* condition */) {
+        /* code */
+      }else if (/* condition */) {
+        /* code */
+      }else{
+
+      }
+  }else if (/* condition */) {
+      if (/* condition */) {
+            /* code */
+      }else if (/* condition */) {
+            /* code */
+      }else{
+
+      }
+  }else{
+
+  }
+
+  return 0;
+}
