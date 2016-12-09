e05a0220485282574e9233b06bdaf33d3476d4c8
藤岡謙 <k.fujioka0727@gmail.com>
2016-12-09 19:12:51 +0900

    20161209 //from home

--- C_C++/201612/ensyuu_sample_6/ensyuu_6_1_3_1.c
+++ C_C++/201612/ensyuu_sample_6/ensyuu_6_1_3_1.c
@@ -3,36 +3,27 @@
 int main(int argc, char const *argv[]) {
   char kamoku;
   int ten;
+  printf("科目を選択して下さい(k:国語、s:数学)　：\n" );
+  scanf("%*c%c", &kamoku );
 
-  printf("科目を選択して下さい(k:国語、s:数学)　：" );
-  scanf(" %c", &kamoku );
-  printf("点数を入力して下さい(0～100)　：" );
-  scanf("%d", &ten );
+  if (kamoku == k) {
+      if (/* condition */) {
+        /* code */
+      }else if (/* condition */) {
+        /* code */
+      }else{
 
-  if (kamoku == 'k') {
-      if (ten >= 73 && ten <= 100 ) {
-        printf("国語の点数は平均点以上です。\n");
       }
-      else if (ten >= 0 && ten < 73) {
-        printf("国語の点数は平均点未満です。\n");
+  }else if (/* condition */) {
+      if (/* condition */) {
+            /* code */
+      }else if (/* condition */) {
+            /* code */
+      }else{
+
       }
-      else{
-        printf("国語の点数の入力エラーです。\n");
-      }
-  }
-  else if (kamoku == 's') {
-      if (ten >= 61 && ten <= 100 ) {
-        printf("数学の点数は平均点以上です。\n");
-      }
-      else if (ten >= 0 && ten < 61) {
-        printf("数学の点数は平均点未満です。\n");
-      }
-      else{
-        printf("数学の点数の入力エラーです。\n");
-      }
-  }
-  else{
-    printf("科目の入力エラーです。\n");
+  }else{
+
   }
 
   return 0;
