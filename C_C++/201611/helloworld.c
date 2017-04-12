811231d99c94b6863108a32d2e6c704c58706e21
fujiokk <k.fujioka0727@gmail.com>
2017-04-12 15:01:44 +0900

    20170412

--- C_C++/201611/helloworld.c
+++ C_C++/201611/helloworld.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char const **argv[]) {
-
-  printf("hello,world!\n");/* hello world*/
+int main(int argc, char const *argv[]) {
+  printf("hello world\n");
   return 0;
 }
